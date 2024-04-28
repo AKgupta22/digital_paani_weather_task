67bdde69d61698f3f5a02c6127972b32d2f0f2f6
AKASH GUPTA <ag565609@gmail.com>
2024-04-28 18:00:37 +0530

    fixed bugs

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://api.openweathermap.org/",
+    baseURL: "https://api.openweathermap.org/",
     params: {
         appid: "342b9f3be9c30100ec4e8f8516a30c47"
     }
